655739e345994c8ddddc372f8f2acbc93dbc76b9
juanestupinan <102682002+juanestupinan@users.noreply.github.com>
2024-05-21 18:44:48 -0500

    image banner configuration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -78,7 +78,7 @@ export default function Navbar() {
       <ul className="flex gap-2">
         <li className={`relative rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer flex-col gap-2`}>
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-home" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-home" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
             <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
@@ -89,7 +89,7 @@ export default function Navbar() {
         </li>
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-briefcase" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-briefcase" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
             <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
@@ -100,7 +100,7 @@ export default function Navbar() {
         </li>
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
             <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
@@ -109,7 +109,7 @@ export default function Navbar() {
         </li>
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-address-book" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-address-book" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <path d="M20 6v12a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2z" />
               <path d="M10 16h6" />
@@ -126,7 +126,7 @@ export default function Navbar() {
       <ul className="flex gap-2">
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
             <path d="M8 11l0 5" />
@@ -138,7 +138,7 @@ export default function Navbar() {
         </li>
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
           </svg>
@@ -146,7 +146,7 @@ export default function Navbar() {
         </li>
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
             <path d="M3 7l9 6l9 -6" />
@@ -158,7 +158,7 @@ export default function Navbar() {
       <ul className="flex gap-2">
         <li className="rounded-full bg-[#212121] drop-shadow w-10 h-10 flex justify-center items-center cursor-pointer">
           <a href="/">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-moon" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#7e7e7e" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-moon" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="#7e7e7e" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
           </svg>
